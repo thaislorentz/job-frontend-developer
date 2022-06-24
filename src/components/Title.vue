2f68c84c47a0d2f6e4cfde3d7226a2e44f14c343
Thais Lorentz <thaisbarcelos.456@gmail.com>
2022-06-24 20:24:45 -0300

    feat: add title component

---
+++ src/components/Title.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="title">
+    <div class="title-line" />
+    <h3 class="title-title">{{title}}</h3>
+    <div class="title-line" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &-line {
+        height: 1px;
+        width: 35%;
+        background: #DEDAF1;
+    }
+    &-title {
+        color: #725BC2;
+        padding: 30px;
+        font-weight: 500;
+    }
+}
+@media (max-width: 650px) {
+    .title {
+    &-line {
+        width: 25%;
+    }
+    }
+}
+</style>
