da4513a22448d3d809a7666bcf3fa341654b89e6
Thais Lorentz <thaisbarcelos.456@gmail.com>
2022-06-24 18:00:00 -0300

    feat: create category navigation

---
+++ src/components/Nav.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav class="nav">
+    <div class="nav-bars" @click="showCategories = !showCategories">
+      <font-awesome-icon icon="bars" />
+      <span class="nav-bars-title">Categorias</span>
+    </div>
+    <ul class="nav-elements" v-if="showCategories">
+      <li
+        class="nav-elements-item"
+        v-for="item in category"
+        :key="item"
+        @click="() => $router.push(`/${item}`)"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import InputText from "../components/inputs/InputText.vue";
+
+export default {
+  name: "Header",
+  components: {
+    InputText
+  },
+  data() {
+    return {
+      category: ["electronic", "jewelery", "mens clothing", "Womens Clothing"],
+      showCategories: true,
+      windowWidth: window.innerWidth
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.nav {
+  width: 100%;
+  background: #725bc2;
+
+  &-bars {
+    display: none;
+  }
+
+  &-elements {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+    color: white;
+    text-transform: capitalize;
+
+    &-item {
+      padding: 10px 50px;
+      cursor: pointer;
+      transition: 0.3s;
+      font-weight: bold;
+    }
+
+    &-item:hover {
+      background: #aa93ff;
+    }
+  }
+}
+
+@media (max-width: 850px) {
+  .nav {
+    &-elements {
+      flex-direction: column;
+    }
+    &-bars {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      color: white;
+      padding: 15px;
+      border-bottom: 1px solid #aa93ff;
+      &-title {
+        margin-left: 10px;
+      }
+    }
+  }
+}
+</style>
