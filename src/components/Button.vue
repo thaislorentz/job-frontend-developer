668a2a57b09015fc6f5b6d27972e5874b9d5c4f4
Thais Lorentz <thaisbarcelos.456@gmail.com>
2022-06-26 09:32:02 -0300

    feat: add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="evt => $emit('click', evt)">{{msg}}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    background: #725bc2;
+    border-radius: 5px;
+    padding: 5px 8px;
+    color: #ffffff;
+    border: none;
+    font-size: 16px;
+}
+
+@media screen and (max-width: 700px) {
+    button {
+        padding: 5px 10px;
+        font-size: 12px;
+    }
+}
+</style>
